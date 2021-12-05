bad3960bb142ed448aed8836a49f5654debee251
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-05 19:22:28 +0500

    login page finished

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,7 +3,13 @@ import "./Input.scss"
 export default function Input(props){
     return (
       <div className="input__wrapper">
-            <input className={`input ${props.className}`} {...props} autoComplete="off"/>
+            <input
+              className={`input ${props?.className ? props?.className : ""}${
+                props.error && "error-input"
+              }`}
+              {...props}
+				autoComplete="off"
+			/>
             <label data-placeholder={props.placeholder}></label>
       </div>
     )
