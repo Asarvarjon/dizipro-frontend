670583bda316c0815d55e518783166a061d60844
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-07 22:00:04 +0500

    checkyouremail section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import CheckYourEmail from './pages/CheckYourEmail/CheckYourEmail';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
 
@@ -8,6 +9,11 @@ function App() {
         <Switch>
              <Route path="/login" component={Login} exact />
              <Route path="/registration" component={Registration} exact />
+             <Route
+						path="/checkyouremail"
+						component={CheckYourEmail}
+						exact
+					/>
 
         </Switch>
   </BrowserRouter>
