a06673905c1ef937ec56172c4f794c100a906ceb
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-09 23:27:28 +0500

    added sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+
 export default function Home() {
     return (
-        <h1>HOME</h1>
+        <main>
+            <Sidebar/>
+        </main>
     )
 }
