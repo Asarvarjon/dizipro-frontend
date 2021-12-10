9c0ecf9c868b8c0c44881aec667bd0adacf1612d
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-10 10:54:00 +0500

    added icons

---
+++ src/components/Header/HomeHeader.jsx
@@ -0,0 +1,10 @@
+import "./HomeHeader.scss";
+
+
+export default function HomeHeader() {
+    return <header className="home-header">
+        <h2 className="home-header__title" >
+            Projects
+        </h2>
+    </header>
+}
