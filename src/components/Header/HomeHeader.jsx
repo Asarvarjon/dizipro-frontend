f5bcec90907ee9020b5340962c847bcc19d768a6
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-10 12:02:47 +0500

    search part was added

--- src/components/Header/HomeHeader.jsx
+++ src/components/Header/HomeHeader.jsx
@@ -1,10 +1,46 @@
 import "./HomeHeader.scss";
+import SearchIcon from "./../../assets/icons/search.jsx"
+import NotificationIcon from "./../../assets/icons/notification.jsx"
+import SearchDropdown from "../SearchDropdown/SearchDropdown";
+import { useState } from "react";
 
 
 export default function HomeHeader() {
+
+    const [searchOpened, setSearchOpened] = useState(false);
+
     return <header className="home-header">
         <h2 className="home-header__title" >
             Projects
         </h2>
+
+        <ul className="home-header__actions-list">
+            <li className="home-header__actions-item">
+                
+                 <SearchDropdown opened={searchOpened} placeholder="Search" />
+                    <button
+                            onClick={() => setSearchOpened(!searchOpened)}
+                            className="home-header__action-link"
+                        > 
+                            <SearchIcon /> 
+					</button>
+            </li>
+            <li className="home-header__actions-item">
+					<button
+						// onClick={() =>
+						// 	setNotificationOpened(!notificationOpened)
+						// }
+						className="home-header__action-link"
+					>
+						<NotificationIcon />
+					</button>
+					{/* <Notification opened={notificationOpened} /> */}
+			</li>
+            <li className="home-header__actions-item">
+					<button className="home-header__action-link"> 
+                        <img src="https://picsum.photos/30" alt="" className="home-header__action-profile-img" />
+					</button> 
+			</li>
+        </ul>
     </header>
 }
