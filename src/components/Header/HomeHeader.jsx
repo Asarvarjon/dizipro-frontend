6f5768ce188a27b99e86624a6151057ccc678f57
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-12-11 18:02:55 +0500

    fixed

--- src/components/Header/HomeHeader.jsx
+++ src/components/Header/HomeHeader.jsx
@@ -1,5 +1,3 @@
-
-  
 import React from "react";
 import NotificationIcon from "../../assets/icons/notification";
 import SearchIcon from "../../assets/icons/search";
--- src/components/SearchDropdown/SearchDropdown.jsx
+++ src/components/SearchDropdown/SearchDropdown.jsx
@@ -1,10 +1,56 @@
 import "./SearchDropdown.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import ArrowImage from "../../assets/icons/arrow.png";
 
+export default function SearchDropdown({ setSearchOpened, opened, ...props }) {
+	const [data, setData] = React.useState([]);
 
-export default function SearchDropdown(props){
-    return (
-        <div className="search-dropdown">
-            <input type="text" className={`search-dropdown__input ${props.opened ? "open" : "close"}`} {...props} />
-        </div>
-    )
+	const handleKeyUp = (e) => {
+		if (e.target.value.length > 0) {
+			setData([
+				{
+					title: "3D MAX",
+					link: "/filter",
+				},
+				{
+					title: "3D MAX",
+					link: "/filter",
+				},
+			]);
+		}
+	};
+
+	return (
+		<div className="search-dropdown">
+			<input
+				onKeyUp={handleKeyUp}
+				type="text"
+				className={`search-dropdown__input ${
+					opened ? "open" : "close"
+				}`}
+				{...props}
+			/>
+
+			<ul className="search-dropdown__list">
+				{data.length === 0 && <p>Type something</p>}
+				{data.map((item, index) => {
+					return (
+						<li key={index} className="search-dropdown__item">
+							<Link to={item.link}>{item.title}</Link>
+							<img src={ArrowImage} alt="Arrow" />
+						</li>
+					);
+				})}
+			</ul>
+
+			<div
+				onClick={(e) => {
+					console.log(opened);
+					setSearchOpened(!opened);
+				}}
+				className="search-dropdown__background"
+			></div>
+		</div>
+	);
 }
